dcbb70d00da4579a5d8b7a13ff513993463d7d47
Si Yong Kim <zzandland@gmail.com>
2018-08-23 11:46:55 -0400

    Initial commit

--- src/recursion.js
+++ src/recursion.js
@@ -7,6 +7,13 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+	if(n < 0) {
+		return null;	
+	} else if(n === 0) {
+		return 1;	
+	} else {
+		return n * factorial(n - 1);	
+	}
 };
 
 // 2. Compute the sum of an array of integers.
